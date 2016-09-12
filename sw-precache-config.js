93d437402f7d259bd5550484ec3ea8af62232b53
jorgecasar <jorge.casar@gmail.com>
2016-09-12 13:29:25 +0200

    chore(sw-precache-config): Add sw precache config

---
+++ sw-precache-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+  ],
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [ /^\/[^\_]+\/?/ ]
+};
