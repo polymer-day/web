695567ec2403d10b1658c338d0f821baa90249e9
Abdón Rodríguez Davila <a@abdonrd.com>
2016-10-05 08:36:46 +0200

    Fix service-worker-precache config (#114)
    The polymer-build don't read the sw-precache-config.js

--- sw-precache-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  staticFileGlobs: [
-    '/index.html',
-    '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
-  ],
-  navigateFallback: '/index.html',
-  navigateFallbackWhitelist: [ /^\/[^\_]+\/?/ ]
-};
